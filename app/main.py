eb3db355fe56974beb1717c730fe89f8cf246aff
imramen07 <chramen07@gmail.com>
2026-06-10 23:22:31 +0530

    feat(main): init FastAPI app, include proxy/dashboard routers

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from app.proxy.router import router
 from app.dash.dash_router import router as dash_router
+from app.storage.database import engine
+from app.storage.models import Base
 
-app = FastAPI()
+#lifespan instead of on_event
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Starting up")
+    Base.metadata.create_all(bind = engine)
+    print("DB tables created")
+    yield
+    print("Shutting down")
+
+app = FastAPI(lifespan = lifespan)
+
 app.include_router(dash_router)
 app.include_router(router)

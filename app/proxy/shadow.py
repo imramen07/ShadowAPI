6ef4cef33548315ab1acd89ba0a7b7f981310af9
imramen07 <chramen07@gmail.com>
2026-06-10 23:19:05 +0530

    feat(shadow) add shadow mode response retrieve from cache

--- app/proxy/shadow.py
+++ app/proxy/shadow.py
@@ -1,20 +1,22 @@
 from fastapi import Response
-from app.storage.crud import getrecord
+from app.storage.crud import get_fresh_record
 from app.core.logger import logger
 
 def get_shadow_response(
         db,
+        tenant_id,
         method,
         path
 ):
-    record = getrecord(
-        db = db,
-        method = method,
-        path = path
+    record = get_fresh_record(
+        db,
+        tenant_id,
+        method,
+        path
     )
 
     if not record:
-        logger.warning(f"Cache Miss: {method} {path}")
+        logger.warning(f"Cache Miss: {tenant_id} - {method} {path}")
         return None
     
     #Returns Response object instead of json
